57acbce5cc70c53e363f55adceb5d3350fd5147d
omnius <gs5m3011@gmail.com>
2020-08-22 23:50:57 +0900

    bindValue use update

--- models/db/model.php
+++ models/db/model.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * DB Function
+ * GNU , PDO USE
  * @author OMNIUS
  * @copyright 2020 OMNIUS
  */
@@ -27,32 +28,32 @@ class Dbf
     $this->dbl = $row_arr;
   }
 
-  public function db_pdo_list($table, $data_arr) // 일반 PDO형식 DB INSERT
+  public function db_pdo_in($table, $data_arr) // 일반 PDO형식 DB INSERT
   {
     $this->dbcon();
-		$connect = $this->dbcon;
-		$sql = "INSERT INTO {$table} ";
-		foreach ($data_arr as $key => $value) {
-			$sql_field .= "{$key},";
-			$sql_value .= ":{$key},";
-		}
-		$sql_field .= "wr_indate";
-		$sql_value .= time();
-		$sql .= "({$sql_field}) VALUE ({$sql_value})";
-		$sql = $connect->prepare($sql);
-		foreach ($data_arr as $key => $value) {
-			if (gettype($value) == "string") {
-				$sql->bindValue(":{$key}", $value, PDO::PARAM_STR);
-			}
-			if (gettype($value) == "integer") {
-			  $sql->bindValue(":{$key}", $value, PDO::PARAM_INT);
-			}
-		}
-		if ($sql->execute()) {
-			return 1;
-		} else {
-			return 0;
-		}
+    $connect = $this->dbcon;
+    $sql = "INSERT INTO {$table} ";
+    foreach ($data_arr as $key => $value) {
+      $sql_field .= "{$key},";
+      $sql_value .= ":{$key},";
+    }
+    $sql_field .= "wr_indate";
+    $sql_value .= time();
+    $sql .= "({$sql_field}) VALUE ({$sql_value})";
+    $sql = $connect->prepare($sql);
+    foreach ($data_arr as $key => $value) {
+      if (gettype($value) == "string") {
+        $sql->bindValue(":{$key}", $value, PDO::PARAM_STR);
+      }
+      if (gettype($value) == "integer") {
+        $sql->bindValue(":{$key}", $value, PDO::PARAM_INT);
+      }
+    }
+    if ($sql->execute()) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 
 }
