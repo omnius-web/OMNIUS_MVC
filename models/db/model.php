7d7cf769c99e31d71a37bc2c66e4c8bdaeb161c1
omnius <gs5m3011@gmail.com>
2020-08-22 13:54:19 +0900

    PDO INSERT Function Update

--- models/db/model.php
+++ models/db/model.php
@@ -6,11 +6,17 @@
  */
 class Dbf
 {
+  private $dbcon; // PDO DB정보
   public $dbl = []; // select row
 
-  function dbl($table, $filter = 0)
+  private function dbcon()
   {
-    $sql = " select * from g5_write_{$table} ";
+		$this->dbcon = new PDO('mysql:host=localhost;dbname=DBNAME', 'root', 'PASSWORD');
+	}
+
+  public function dbl($table, $filter = 0) // 그누보드5 DB LIST
+  {
+    $sql = " select * from {$table} ";
     if ($filter !== 0) {
       $sql .= "where {$filter} ";
     }
@@ -19,6 +25,32 @@ class Dbf
       $row_arr[] = $row;
     }
     $this->dbl = $row_arr;
+  }
+
+  public function db_pdo_list($table, $data_arr) // 일반 PDO형식 DB INSERT
+  {
+    $this->dbcon();
+		$connect = $this->dbcon;
+		$sql = "INSERT INTO {$table} ";
+		foreach ($data_arr as $key => $value) {
+			$sql_field .= "{$key},";
+			if (gettype($value) == "string") {
+				$sql_value .= "'{$value}',";
+			}
+			if (gettype($value) == "integer") {
+			  $sql_value .= "{$value},";
+			}
+		}
+		$sql_field .= "wr_5";
+		$sql_value .= "now()";
+		$sql .= "({$sql_field}) VALUE ({$sql_value})";
+		$sql = $connect->prepare($sql);
+
+		if ($sql->execute()) {
+			print "<script>alert('등록되었습니다.'); window.location.replace('adm.php');</script>";
+		} else {
+			print "<script>alert('입력오류.'); window.location.replace('adm.php');</script>";
+		}
   }
 }
 
--- models/ex_om/model.php
+++ models/ex_om/model.php
@@ -1,6 +1,5 @@
 <?php
 require_once "{$_SERVER["DOCUMENT_ROOT"]}/models/db/model.php";
-
 /**
  *
  */
