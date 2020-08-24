4b13d08dc7e876d741ec9e3a443f2a46b166f936
omnius <gs5m3011@gmail.com>
2020-08-24 10:38:50 +0900

    함수 입력값 배열인지 확인 추가 select list 추가

--- models/db/model.php
+++ models/db/model.php
@@ -28,29 +28,59 @@ class Dbf
     $this->dbl = $row_arr;
   }
 
-  public function db_pdo_in($table, $data_arr) // 일반 PDO형식 DB INSERT
+  public function db_pdo_in($table, $data_arr = 0) // 일반 PDO형식 DB INSERT
   {
     $this->dbcon();
     $connect = $this->dbcon;
-    $sql = "INSERT INTO {$table} ";
-    foreach ($data_arr as $key => $value) {
-      $sql_field .= "{$key},";
-      $sql_value .= ":{$key},";
-    }
-    $sql_field .= "wr_indate";
-    $sql_value .= time();
-    $sql .= "({$sql_field}) VALUE ({$sql_value})";
-    $sql = $connect->prepare($sql);
-    foreach ($data_arr as $key => $value) {
-      if (gettype($value) == "string") {
-        $sql->bindValue(":{$key}", $value, PDO::PARAM_STR);
+    if ($data_arr && is_array($data_arr)) { // $data_arr 비어있지않고 배열인지 검사
+      $sql = "INSERT INTO {$table} ";
+      foreach ($data_arr as $key => $value) {
+        $sql_field .= "{$key},";
+        $sql_value .= ":{$key},";
       }
-      if (gettype($value) == "integer") {
-        $sql->bindValue(":{$key}", $value, PDO::PARAM_INT);
+      $sql_field .= "wr_indate";
+      $sql_value .= time();
+      $sql .= "({$sql_field}) VALUE ({$sql_value})";
+      $sql = $connect->prepare($sql);
+      foreach ($data_arr as $key => $value) {
+        if (gettype($value) == "string") {
+          $sql->bindValue(":{$key}", $value, PDO::PARAM_STR);
+        }
+        if (gettype($value) == "integer") {
+          $sql->bindValue(":{$key}", $value, PDO::PARAM_INT);
+        }
+      }
+      if ($sql->execute()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    } else {
+      return 0;
+    }
+  }
+
+  public function db_pdo_list($table, $where, $where_arr = 0)
+  {
+    $this->dbcon();
+    $connect = $this->dbcon;
+    $sql = "SELECT * FROM {$table}";
+    if ($where_arr && is_array($where_arr)) { // $where_arr 비어있지않고 배열인지 검사
+      $sql .= " where {$where}";
+    }
+    $sql = $connect->prepare($sql);
+    if ($where_arr && is_array($where_arr)) {
+      foreach ($where_arr as $key => $value) {
+        if (gettype($value) == "string") {
+          $sql->bindValue(":{$key}", $value, PDO::PARAM_STR);
+        }
+        if (gettype($value) == "integer") {
+          $sql->bindValue(":{$key}", $value, PDO::PARAM_INT);
+        }
       }
     }
     if ($sql->execute()) {
-      return 1;
+      return $sql->fetchAll();
     } else {
       return 0;
     }
