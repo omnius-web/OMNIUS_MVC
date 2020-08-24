cbd41c8c6a7af3946c28e3a7fc83cca5d0d53ca3
omnius <gs5m3011@gmail.com>
2020-08-24 10:42:44 +0900

    DB PDO 사용방법 update

--- views/db/view.php
+++ views/db/view.php
@@ -9,9 +9,10 @@ $arr['wr_4'] = 2267890;
 (int)$rst = $omvc->db_pdo_in('as_list', $arr);
 var_dump($rst);
 
-$sql = "wr_1 = :wr_1";
+$sql = "wr_1 = :wr_1 and wr_indate = :wr_indate";
 $sql_pr = array(
-  'wr_1' => "test2"
+  'wr_1' => "test2",
+  'wr_indate' => 1234556
 );
 $rst = $omvc->db_pdo_list('as_list', $sql, $sql_pr);
 var_dump($rst);
