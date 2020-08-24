ba33188316a4a07614def12d3f572474e0e9fa25
omnius <gs5m3011@gmail.com>
2020-08-24 10:40:53 +0900

    DB PDO 사용방법

---
+++ views/db/view.php
@@ -0,0 +1,18 @@
+<?php
+require_once "{$_SERVER["DOCUMENT_ROOT"]}/models/db/model.php";
+$omvc = new Dbf();
+
+$arr['wr_1'] = "test2";
+$arr['wr_2'] = "012-1234-1234";
+$arr['wr_3'] = 2212345;
+$arr['wr_4'] = 2267890;
+(int)$rst = $omvc->db_pdo_in('as_list', $arr);
+var_dump($rst);
+
+$sql = "wr_1 = :wr_1";
+$sql_pr = array(
+  'wr_1' => "test2"
+);
+$rst = $omvc->db_pdo_list('as_list', $sql, $sql_pr);
+var_dump($rst);
+?>
